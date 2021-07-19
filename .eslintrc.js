6ba5c26f13875091335242cfaa891544128f97c4
Liam Rella <liam.rella@gmail.com>
2021-07-20 00:12:47 +0930

    lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ["airbnb-base", "prettier"],
-  parser: "@babel/eslint-parser",
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2020,
     requireConfigFile: false,
@@ -11,71 +11,71 @@ module.exports = {
     jest: true,
   },
   rules: {
-    "no-debugger": 0,
-    "no-alert": 0,
-    "no-await-in-loop": 0,
-    "no-return-assign": ["error", "except-parens"],
-    "no-restricted-syntax": [
+    'no-debugger': 0,
+    'no-alert': 0,
+    'no-await-in-loop': 0,
+    'no-return-assign': ['error', 'except-parens'],
+    'no-restricted-syntax': [
       2,
-      "ForInStatement",
-      "LabeledStatement",
-      "WithStatement",
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
     ],
-    "no-unused-vars": [
+    'no-unused-vars': [
       1,
       {
         ignoreRestSiblings: true,
-        argsIgnorePattern: "res|next|^err",
+        argsIgnorePattern: 'res|next|^err',
       },
     ],
-    "prefer-const": [
-      "error",
+    'prefer-const': [
+      'error',
       {
-        destructuring: "all",
+        destructuring: 'all',
       },
     ],
-    "arrow-body-style": [2, "as-needed"],
-    "no-unused-expressions": [
+    'arrow-body-style': [2, 'as-needed'],
+    'no-unused-expressions': [
       2,
       {
         allowTaggedTemplates: true,
       },
     ],
-    "no-param-reassign": [
+    'no-param-reassign': [
       2,
       {
         props: false,
       },
     ],
-    "no-console": 0,
-    "import/prefer-default-export": 0,
+    'no-console': 0,
+    'import/prefer-default-export': 0,
     import: 0,
-    "func-names": 0,
-    "space-before-function-paren": 0,
-    "comma-dangle": 0,
-    "max-len": 0,
-    "import/extensions": 0,
-    "no-underscore-dangle": 0,
-    "consistent-return": 0,
-    "no-shadow": [
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    'no-shadow': [
       2,
       {
-        hoist: "all",
-        allow: ["resolve", "reject", "done", "next", "err", "error"],
+        hoist: 'all',
+        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
       },
     ],
     quotes: [
       2,
-      "single",
+      'single',
       {
         avoidEscape: true,
         allowTemplateLiterals: true,
       },
     ],
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        trailingComma: "es5",
+        trailingComma: 'es5',
         useTabs: false,
         tabWidth: 2,
         semi: true,
@@ -83,5 +83,5 @@ module.exports = {
       },
     ],
   },
-  plugins: ["html", "prettier"],
+  plugins: ['html', 'prettier'],
 };
