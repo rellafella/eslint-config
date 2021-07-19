387d5040769deedff29ac257420cc3320e07701b
Liam Rella <liam.rella@gmail.com>
2021-07-20 01:14:50 +0930

    update stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,19 +1,32 @@
+const tailwindConfig = require('stylelint-config-tailwindcss');
+
+const tailwindAtRules =
+  tailwindConfig.rules['at-rule-no-unknown'][1].ignoreAtRules;
+
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-standard', 'stylelint-config-tailwindcss'],
   rules: {
     'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: [
-          'tailwind',
-          'apply',
-          'variants',
-          'responsive',
-          'screen',
-        ],
+        ignoreAtRules: [...tailwindAtRules, 'if', 'else'],
+      },
+    ],
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: ['after-same-name', 'first-nested', 'inside-block'],
+        ignoreAtRules: ['if', 'else'],
+      },
+    ],
+    'block-closing-brace-newline-after': [
+      'always',
+      {
+        ignoreAtRules: ['if', 'else'],
       },
     ],
     'declaration-block-trailing-semicolon': null,
     'no-descending-specificity': null,
+    'string-quotes': ['single', { avoidEscape: true }],
   },
 };
